d76985c7f933bf79779e6d3f3b8d3a9a865956f4
bboy22dxdavid <https://github.com/bboy22dxdavid/Clone_flutter_Soccer.git>
2023-07-28 19:55:04 -0300

    finalizando a logica para incrementar e decrementar os botoes

--- lib/main.dart
+++ lib/main.dart
@@ -9,97 +9,127 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
 
-  //funcao de decrementação
+
+class HomePage extends StatefulWidget {
+   HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  //funcao de decremental
   void decrement() {
-    // debugando a funcao
-    print("decrementando");
+    setState(() {
+      // debugando a funcao
+      print("decrement ${count}");
+      count--;
+    });
+
   }
 
-  //funcao de incrementando
+  //funcao de incremental
   void increment() {
-    // debugando a funcao
-    print("incrementando");
+    setState(() {
+      // debugando a funcao
+      print("incremental = ${count}");
+      count++;
+    });
+
   }
+
+  bool get isEmpty => count == 0;
+  bool get isFull => count == 20;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            "Pode entrar!",
-            style: TextStyle(
-              fontSize: 26,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/sorvete.jpg"),
+            fit: BoxFit.cover
           ),
-          const Padding(
-            padding: EdgeInsets.all(32),
-            child:  Text(
-              "0",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 100,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+             Text(
+              isFull ? "Lotado" :  "Pode entrar!",
+              style: const  TextStyle(
+                fontSize: 30,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100, 100),
-                  primary: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                ),
-                child: const Text(
-                  "Saiu",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
+             Padding(
+              padding: const EdgeInsets.all(32),
+              child: Text(
+                count.toString(),
+                style:  TextStyle(
+                  color: isFull ? Colors.red.withOpacity(0.5) : Colors.white,
+                  fontSize: 100,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              const SizedBox(
-                width: 32,
-              ),
-              TextButton(
-                onPressed: increment,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  fixedSize: const Size(100, 100),
-                  primary: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(24),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: isEmpty ? null : decrement,
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: isEmpty ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  child: const Text(
+                    "Saiu",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
                   ),
                 ),
-                child: const Text(
-                  "Entrou",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
+                const SizedBox(
+                  width: 32,
+                ),
+                TextButton(
+                  onPressed: isFull ? null :increment,
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    backgroundColor: isFull ? Colors.white.withOpacity(0.2) : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  child: const Text(
+                    "Entrou",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
